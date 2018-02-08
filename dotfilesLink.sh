3e0c97baf26ecd9eb391315ea0ccd7d4c106487e
Panarchy <harhogefoo@users.noreply.github.com>
2018-02-08 10:26:25 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/colors/colors ~/.vim
+ln -sf ~/dotfiles/.git-completion.bash ~/.git-completion.bash
+ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
